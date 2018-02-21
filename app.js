9922deeb83ded5b3da4c6af981b2d304da15fbae
Ralph <ralphrpo@gmail.com>
2018-02-21 18:49:33 +0000

    Test HTML page

--- app.js
+++ app.js
@@ -3,27 +3,38 @@ console.log(process.env.SHIPPO_KEY)
 // Load Node Modules
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
+
 
 // Load Local Modules
 var freight = require('./modules/freight');
 
 var app = express();
 
-//To Support body on post requests
+//To Support body on income POST requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Provides access to asset folder
+app.use(express.static('public'));
 
 //Endpoint to Retrieve Rates Prices
 app.post('/Rates', function (req, res) {
     console.log("Received POST Request")
-    console.log(JSON.stringify(req.body))
+    console.log(req.body)
     freight.Rates(req.body, function (err, resp) {
         if (err) {
             res.status(500).send(err)
         } else {
+            console.dir(resp);
             res.status(200).send(resp)
         }
     })
+});
+
+// Root path to retrieve Index.html (test page)
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 //Gets port from environment of use default
